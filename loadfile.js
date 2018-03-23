41d833b66812e9e6378b7b4c75c8f42b64bd58d7
ZHDou <1178647512@qq.com>
2018-03-23 22:25:13 +0800

    Update loadfile.js

--- loadfile.js
+++ loadfile.js
@@ -1,5 +1,6 @@
 
 d3.tsv("demo.tsv",function(error,tsvdata){  
+    document.write("hello");
     console.log(tsvdata);  
     var str = d3.tsv.format( tsvdata );           
     console.log(str.length);  
