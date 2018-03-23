8e4275028467e53f081be5549a3e7834e74d0bb4
ZHDou <1178647512@qq.com>
2018-03-23 22:44:32 +0800

    Delete loadfile.js

--- loadfile.js
+++
@@ -1,8 +0,0 @@
-
-d3.tsv("demo.tsv",function(error,tsvdata){  
-    document.write("hello");
-    console.log(tsvdata);  
-    var str = d3.tsv.format( tsvdata );           
-    console.log(str.length);  
-    console.log(str);     
-});  
